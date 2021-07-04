07cfa7fadd8060782b35f9c318a01fdc27da77f9
Krutika Patil <krutiks89@gmail.com>
2021-07-03 20:40:30 -0400

    Added first styled components

--- styled-comp-intro/src/App.jsx
+++ styled-comp-intro/src/App.jsx
@@ -1,8 +1,28 @@
+import styled from "styled-components";
+
+const BasicTitle = styled.h1`
+  text-transform: capitalize;
+  text-align: center;
+`;
+
+const DefaultButton = styled.button`
+  display: block;
+  margin: 1rem auto;
+  background-color: blueviolet;
+  color: #fff;
+  border: none;
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  width: 200px;
+  text-transform: capitalize;
+  cursor: pointer;
+`;
+
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
-      <h2 className="title">Styled Components</h2>
-      <button className="btn">Click me</button>
+      <BasicTitle>Styled components</BasicTitle>
+      <DefaultButton>click me</DefaultButton>
     </div>
   );
 }
